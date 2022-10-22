34fd0c4aa502b97225a7141696c3b32cf2c2dd68
Pravinkumar Singh <pravinkumarosingh@gmail.com>
2022-10-22 15:34:33 +0530

    constructor based injection in BusinessService.

--- src/main/java/com/sugarglider/spring/enterprise/BusinessService.java
+++ src/main/java/com/sugarglider/spring/enterprise/BusinessService.java
@@ -3,12 +3,17 @@ package com.sugarglider.spring.enterprise;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.xml.crypto.Data;
 import java.util.List;
 
 @Component
 public class BusinessService {
     @Autowired
     private DataService dataService;
+
+    public BusinessService(DataService dataService){
+        this.dataService = dataService;
+    }
     public long calculateSum(){
         List<Integer> data = dataService.getData();
         return data.stream().reduce(Integer::sum).get();
